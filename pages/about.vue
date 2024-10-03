d956b4936f9f87b541f4c066d4a8bf8338d226a0
Liuzhch1 <liuhiheng@outlook.com>
2024-10-03 13:14:29 +0800

    feat: about page

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>
+      It's too annoying to send files through Airdrop or whatever, so I made
+      this.<br />
+      Hope you find it useful.
+    </h1>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
